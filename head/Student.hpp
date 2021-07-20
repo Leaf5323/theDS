e55ab8869388b0897d10d60224d25129199f5d77
cuc2020212063028 <74447220+cuc2020212063028@users.noreply.github.com>
2021-07-20 09:49:50 +0800

    Update Student.hpp

--- head/Student.hpp
+++ head/Student.hpp
@@ -23,3 +23,51 @@ string Student::getStudentName(){
 int Student::getStudentScore(){
     return this->studentScore;
 }
+
+    
+void FindStudent(SqList*L)
+{
+    int n;
+    printf("请输入查找方式: \n0=学号查找\n1=储存位置查找");//查找方式的选择
+    scanf("%d",&n);
+    printf("请输入要查找的学生信息:");
+    switch (n) {
+        case 0:
+            {
+                long number;//输入需要查找的学生学号
+                scanf("%ld",&number);
+                for (int i=0;i<=L->length;i++)
+                {
+                    if(number==L->student[i].id)
+                        PrintStudent(L,i);
+                }
+                break;
+            }
+        case 1:
+            {
+                int number;
+                scanf("%d",&number);
+                PrintStudent(L,number-1);
+                break;
+            }
+        default:
+            printf("您输入的查找方式有误请确认");
+            break;
+    }
+    void DeletStudent (Sqlist*L,int i)
+{
+    if(i<0||i>sum)
+        printf("输入信息有误");
+    else
+    {
+        for (int m=i-1;m<sum;m++)
+        {
+            L->student[m]=L->student[m+1];        
+        }//删除
+        sum--;
+        L->length--;
+    }
+    //更新表中的学生信息
+void ReGetStudent(Sqlist*L)
+{
+    int p;
